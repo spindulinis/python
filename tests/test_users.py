a021937abda6b905a7fa552cc16b95421d0f0001
spindulinis <benasnedzviega@gmail.com>
2026-02-24 20:56:33 +0200

    test user list

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,21 @@
 from fastapi.testclient import TestClient
 from fastapi import status
 
+def test_read_users(client: TestClient):
+    for i in range(3):
+        client.post("/api/v1/users/", json={
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": f"email{i}@example.com", 
+            "password": "password123"
+        })
+
+    response = client.get("/api/v1/users/?limit=1")
+    data = response.json()
+    
+    assert data["count"] == 3      
+    assert len(data["data"]) == 1
+
 def test_create_user_flow(client: TestClient):
     response = client.post(
         "/api/v1/users/",
