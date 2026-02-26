1b968479f3cadab777a1e2d6ac6f1f07448799d7
spindulinis <benasnedzviega@gmail.com>
2026-02-26 13:44:12 +0200

    test public products

---
+++ tests/test_public_products.py
@@ -0,0 +1,41 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+
+def test_read_products(client: TestClient):
+    client.post(
+        "/products/",
+        json={
+            "title": "Star Wars",
+            "description": "Movie about space wars",
+        },
+    )
+
+    response = client.get("/public-products/?limit=1")
+    data = response.json()
+    
+    assert data["count"] == 1      
+    assert len(data["data"]) == 1
+
+    result = data["data"][0]
+
+    assert isinstance(result["id"], int)
+    assert result["title"] == "Star Wars"
+    assert result["description"] == "Movie about space wars"
+
+def test_read_product_by_id(client: TestClient):
+    create_response = client.post(
+        "/products/",
+        json={
+            "title": "Star Wars",
+            "description": "Movie about space wars",
+        },
+    )
+    product_id = create_response.json()["id"]
+
+    response = client.get(f"/public-products/{product_id}")
+    
+    assert response.status_code == status.HTTP_200_OK
+    result = response.json()
+    assert result["id"] == product_id
+    assert result["title"] == "Star Wars"
+    assert result["description"] == "Movie about space wars"
